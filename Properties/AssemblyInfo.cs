6088bb9069cfbe6eec08804428390b23926eaac2
TrueThatDev <gamingguy444@gmail.com>
2017-05-17 23:42:17 +0300

    fix ref???

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Awesome Network")]
 [assembly: AssemblyProduct("UconomyShop")]
-[assembly: AssemblyCopyright("Copyright © Zamirathe,TrueThat 2017")]
+[assembly: AssemblyCopyright("Copyright © TrueThat , Zamirathe 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
